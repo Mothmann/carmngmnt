a6d75eb59b70f38ab148f4d7f0ca59da8dfd9d4f
Othman <56235490+Mothmann@users.noreply.github.com>
2021-03-17 23:44:31 +0100

    auth works

--- src/fleetmanagmentsys1/FleetManagmentsys1.java
+++ src/fleetmanagmentsys1/FleetManagmentsys1.java
@@ -9,39 +9,66 @@ import java.util.ArrayList;
 public class FleetManagmentsys1 {
 
     private static final String USERNAME= "root";
-    private static final String PASSWORD= "qwerty";
+    private static final String PASSWORD= "";
     private static final String CONN_STRING=
             "jdbc:mysql://localhost:3306/fleetmanagmentsys1";
-        private int userId; 
-    protected String userPassword;
+        private int IdUtilisateur; 
+    protected String password;
+
+    public FleetManagmentsys1(int IdUtilisateur, String password) {
+        this.IdUtilisateur = IdUtilisateur;
+        this.password = password;
+    }
+    
     public static ArrayList<authentification> studentsList = new ArrayList<authentification>();
 
-
-    public int getUserId() {
-        return userId;
+    public int getIdUtilisateur() {
+        return IdUtilisateur;
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public void setIdUtilisateur(int IdUtilisateur) {
+        this.IdUtilisateur = IdUtilisateur;
     }
 
-    public String getUserPassword() {
-        return userPassword;
+    public String getPassword() {
+        return password;
     }
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
+    public void setPassword(String password) {
+        this.password = password;
     }
+    
+
+
 
     public static void main(String[] args) {
+        auth(1, "azerty");
+        
+    }
+    public static void auth(int IdUtilisateur,String password){
         Connection conn = null;
         try{
             conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
             System.out.println("Connected");
             Statement stmt = (Statement) conn.createStatement();
-            String q = "SELECT * FROM user";
-            ResultSet rs=stmt.executeQuery(q);
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM user where IdUtilisateur = ? and password = ?");
+            pst.setInt(1, IdUtilisateur);
+            pst.setString(2, password);
+            ResultSet rs=pst.executeQuery();
             if(rs.next()){ 
+                System.out.println("yes");
+                }
+            else{
+                System.out.println("no");
+            }
+            conn.close();
+
+        }
+        catch (SQLException e){
+            System.err.println(e);
+        }
+        /*
+          if(rs.next()){ 
                 do{
                 System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3)+","+rs.getString(4)+","+rs.getString(5));
                 }
@@ -56,6 +83,7 @@ public class FleetManagmentsys1 {
         catch (SQLException e){
             System.err.println(e);
         }
+        */
     }
 }
     
